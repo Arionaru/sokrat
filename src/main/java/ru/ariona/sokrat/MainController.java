615d015798c265d12903add53e9d25d845c609d9
СапоГ <katenochekspb@mail.ru>
2019-06-06 11:06:20 +0300

    v0.1 deploy

--- src/main/java/ru/ariona/sokrat/MainController.java
+++ src/main/java/ru/ariona/sokrat/MainController.java
@@ -15,7 +15,7 @@ public class MainController {
     @Autowired
     SokratService sokratService;
 
-    private final String host = "https://sokratitel.herokuapp.com";
+    private final String host = "https://sokratitel.herokuapp.com/";
 
     @GetMapping("/")
     public String mainPage() {
